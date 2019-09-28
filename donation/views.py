eae887377bb4eb2948f43e316124c8975eaa99ae
Agata Orzechowska <agata9orzechowska@gmail.com>
2019-09-28 16:37:54 +0200

    Navigation

--- donation/views.py
+++ donation/views.py
@@ -1,10 +1,18 @@
+from django.db.models import Count
 from django.shortcuts import render
 from django.views import View
+from .models import Donation, Institution, Category
 
 
 class LandingPage(View):
     def get(self, request):
-        return render(request, 'index.html')
+        donations = Donation.objects.all()
+        quantity = 0
+        for donation in donations:
+            quantity += donation.quantity
+        supported_institutions = len(list(set([d.institution for d in donations])))
+        return render(request, 'index.html', {'quantity': quantity,
+                                              'institutions': supported_institutions})
 
 
 class AddDonation(View):
